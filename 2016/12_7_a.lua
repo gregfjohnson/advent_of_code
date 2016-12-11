03f065fa30d9c84c0c0b87ff0948a7ed80b1f9d2
user <none@none>
2016-12-10 23:21:43 -0800

    12/7 part a solved..

---
+++ 2016/12_7_a.lua
@@ -0,0 +1,36 @@
+function haspal(str)
+    if str == nil then return false end
+
+    for i = 1, #str-3 do
+        if str:sub(i,i) == str:sub(i+3,i+3)
+        and str:sub(i+1,i+1) == str:sub(i+2,i+2)
+        and str:sub(i,i) ~= str:sub(i+1,i+1)
+        then
+            return true
+        end
+    end
+
+    return false
+end
+
+function ok(str)
+    for s in str:gmatch('%[([a-z]*)%]') do
+        if haspal(s) then return 0 end
+    end
+    for s in str:gmatch('%]([a-z]*)%[') do
+        if haspal(s) then return 1 end
+    end
+
+    -- start of string
+    if haspal(str:match('^([^%[]*)')) then return 1 end
+
+    -- end of string
+    if haspal(str:match('([^%[]*)$')) then return 1 end
+    return 0
+end
+
+count = 0
+for l in io.lines(arg[1]) do
+    count = count + ok(l)
+end
+print(count)
